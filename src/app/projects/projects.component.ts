121f1dec44cdbdc067bc85e4e96d04548c7959ca
M Gosschalk <monica.gosschalk@btinternet.com>
2017-09-12 14:49:39 +0100

    added details on project page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeAnimation } from '../shared/animations';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  animations: [ fadeAnimation ]
 })
 export class ProjectsComponent implements OnInit {
 
