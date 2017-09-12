3c0f4a8758a2947bacf65cce569cbce9de86672d
M Gosschalk <monica.gosschalk@btinternet.com>
2017-09-12 15:49:21 +0100

    created a responsive projects page

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { fadeAnimation } from '../shared/animations';
+import { ObservableMedia } from '@angular/flex-layout';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-projects',
@@ -9,9 +12,38 @@ import { fadeAnimation } from '../shared/animations';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+
+
+  public cols: Observable<number>;
+
+  constructor(private observableMedia: ObservableMedia) { }
 
   ngOnInit() {
+    //this.cols = 3;
+    // set cols
+     if (this.observableMedia.isActive("xs")) {
+       this.cols = Observable.of(1);
+     } else if (this.observableMedia.isActive("sm") || this.observableMedia.isActive("md")) {
+       this.cols = Observable.of(2);
+     } else if (this.observableMedia.isActive("lg") || this.observableMedia.isActive("xl")) {
+       this.cols = Observable.of(2);
+     }
+
+     // observe changes
+     this.observableMedia.asObservable()
+     .subscribe(change => {
+       switch (change.mqAlias) {
+         case "xs":
+           return this.cols = Observable.of(1);
+         case "sm":
+           return this.cols = Observable.of(2);
+         case "md":
+           return this.cols = Observable.of(2);
+         case "lg":
+         case "xl":
+           return this.cols = Observable.of(2);
+       }
+     });
   }
 
 }
